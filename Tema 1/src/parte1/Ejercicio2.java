5a475a86393733360e70c8f2c2a7e4e549016fbf
HUGO <HUGO@DESKTOP-16VKAHA>
2024-09-23 22:42:01 +0200

    g

--- Tema 1/src/parte1/Ejercicio2.java
+++ Tema 1/src/parte1/Ejercicio2.java
@@ -5,16 +5,22 @@ import java.util.Scanner;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
+		
 		// 1º Declaramos una variable
 		double numero;
+		
 		// 2º Creamos un nuevo Scanner
 		Scanner sc = new Scanner(System.in);
+		
 		// 3º Le pedimos la edad al usuario
 		System.out.println("Dime tu edad");
+		
 		// 4º Obtenemos el numero y lo aplicamos a una variable
 		numero = sc.nextDouble();
+		
 		// 5º Leemos el numero de la variable sumandole uno al resultado
 		System.out.println("El proximo año tendras " + (numero + 1));
+		
 		// 6º Cerramos el Scanner
 		sc.close();
 			}
