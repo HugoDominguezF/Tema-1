c786d32902d7c4db1c5c8db4946af1e810473faf
hugo.dominguez <hugo.dominguez@106-08.info2>
2024-09-25 10:24:55 +0200

    a

--- Tema 1/src/parte1/Ejercicio14.java
+++ Tema 1/src/parte1/Ejercicio14.java
@@ -7,23 +7,15 @@ public class Ejercicio14 {
 	public static void main(String[] args) {
 		
 		double notas1;
-		
 		double notas2;
-		
 		double notas3;
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("¿Cuanta nota tienes en el primer trimestre?");
+		System.out.println("¿Cuales son tus notas?");
 		
 		notas1 = sc.nextDouble();
-		
-		System.out.println("¿Cuanta nota tienes en el segundo trimestre?");
-		
 		notas2 = sc.nextDouble();
-	
-		System.out.println("¿Cuanta nota tienes en el tercer trimestre?");
-		
 		notas3 = sc.nextDouble();
 		
 		System.out.println("La nota media es " + (int)(notas1+notas2+notas3)/3 + ", en el expediente academico sera " + (notas1+notas2+notas3)/3);
