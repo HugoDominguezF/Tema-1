6294b3b81b48aa64d1e13bfc781a197204ef89ed
hugo.dominguez <hugo.dominguez@106-08.info2>
2024-09-26 14:30:35 +0200

    ej

---
+++ Tema 1/src/parte2/Ejercicio1.java
@@ -0,0 +1,21 @@
+package parte2;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		double numero;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime un numero y lo redondeare");
+		numero = sc.nextDouble();
+		
+		System.out.println("Tu numero redondeado es " + (int)(numero+0.5));
+		
+		sc.close();
+	}
+
+}
